c18f41ede7552f6fe2760d12eeec7c241ac88aca
longliveakim98 <mohdaimanhakim.98@gmail.com>
2024-05-16 21:20:25 +0800

    V2

--- script.js
+++ script.js
@@ -1,37 +1,60 @@
-const inputBox = document.getElementById('input-box');
-const listContainer =document.getElementById('list-container');
+document.addEventListener('DOMContentLoaded', () => {
+    const inputBox = document.getElementById('input-box');
+    const listContainer = document.getElementById('list-container');
 
-function addTask(){
-    if(inputBox.value===''){
-        alert("Empty Task");
-    }else{
+    // Fetch todos from JSONPlaceholder
+    fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(todo => {
+                addTaskToDOM(todo.title, todo.completed);
+            });
+        });
+
+    // Add new task
+    window.addTask = function () {
+        if (inputBox.value === '') {
+            alert("Empty Task");
+        } else {
+            addTaskToDOM(inputBox.value, false);
+            inputBox.value = '';
+            saveData();
+        }
+    };
+
+    // Add task to the DOM
+    function addTaskToDOM(task, completed) {
         let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
+        li.innerHTML = task;
+        if (completed) {
+            li.classList.add("checked");
+        }
         listContainer.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
-    inputBox.value='';
-    saveData();
-}
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData()
-    }else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove()
-        saveData()
+    // Handle task click events
+    listContainer.addEventListener("click", function (e) {
+        if (e.target.tagName === "LI") {
+            e.target.classList.toggle("checked");
+            saveData();
+        } else if (e.target.tagName === "SPAN") {
+            e.target.parentElement.remove();
+            saveData();
+        }
+    }, false);
+
+    // Save data to local storage
+    function saveData() {
+        localStorage.setItem("data", listContainer.innerHTML);
     }
-}, false);
 
-function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML);
-}
+    // Show tasks from local storage
+    function showTask() {
+        listContainer.innerHTML = localStorage.getItem("data");
+    }
 
-function showTask(){
-    listContainer.innerHTML = localStorage.getItem("data");
-}
-
-showTask();
+    showTask();
+});
